6525cc2110d3b92ca863d220cb863233f8fd6b62
daniocen777 <danielochoa@outlook.com>
2021-10-10 10:56:04 -0500

    Modificando página principal para usar las paginas responsive

--- web_design/lib/main.dart
+++ web_design/lib/main.dart
@@ -1,23 +1,32 @@
 import 'package:flutter/material.dart';
 
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:web_design/widgets/small_screen.dart';
+
+import '../helpers/responsive.dart';
+import '../widgets/largue_screen.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Web Design',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
-      ),
-    );
+    return GetMaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Dash',
+        theme: ThemeData(
+            scaffoldBackgroundColor: Colors.white,
+            textTheme: GoogleFonts.mulishTextTheme(Theme.of(context).textTheme)
+                .apply(bodyColor: Colors.black87),
+            pageTransitionsTheme: PageTransitionsTheme(builders: {
+              TargetPlatform.iOS: FadeUpwardsPageTransitionsBuilder(),
+              TargetPlatform.android: FadeUpwardsPageTransitionsBuilder(),
+            }),
+            primaryColor: Colors.blue),
+        home: ResponsiveWidwet(
+          largueScreen: LargueScreen(),
+          smallScreen: SmallScreen(),
+        ));
   }
 }
