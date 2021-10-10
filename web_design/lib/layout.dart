3a93985ae30e6fe134d4b989457fc39e362cdd08
daniocen777 <danielochoa@outlook.com>
2021-10-10 10:55:34 -0500

    Creando layout

---
+++ web_design/lib/layout.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/largue_screen.dart';
+
+class SiteLayout extends StatelessWidget {
+  const SiteLayout({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0.0,
+        backgroundColor: Colors.white,
+      ),
+      body: LargueScreen(),
+    );
+  }
+}
