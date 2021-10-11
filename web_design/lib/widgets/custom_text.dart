9e357d9d87e92b163d3d7e4f5b3c84f29a72464c
daniocen777 <danielochoa@outlook.com>
2021-10-11 09:12:37 -0500

    Creando texto personaliado para usarlo en en appBar

---
+++ web_design/lib/widgets/custom_text.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class CustomerText extends StatelessWidget {
+  final String text;
+  final double size;
+  final Color color;
+  final FontWeight fontWeight;
+
+  const CustomerText(
+      {Key? key,
+      required this.text,
+      this.size = 16.0,
+      this.color = Colors.black87,
+      this.fontWeight = FontWeight.normal})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(this.text,
+        style: TextStyle(
+            fontSize: this.size,
+            color: this.color,
+            fontWeight: this.fontWeight));
+  }
+}
