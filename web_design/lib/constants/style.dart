53f1cd5bcc069bacc1dff3ff621c17b82021092c
daniocen777 <danielochoa@outlook.com>
2021-10-11 09:09:42 -0500

    Agregando estilo-colores

---
+++ web_design/lib/constants/style.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/widgets.dart' show Color;
+
+final Color light = const Color(0xfff7f8fc);
+final Color lightGrey = const Color(0xffa4a6b3);
+final Color dark = const Color(0xff363740);
+final Color active = const Color(0xff3c19c0);
