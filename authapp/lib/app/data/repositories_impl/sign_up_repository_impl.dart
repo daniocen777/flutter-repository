d4c1041a5b2c4dc65b13e059145b095031126b53
daniocen777 <danielochoa@outlook.com>
2022-01-15 10:31:03 -0500

    Clase que implementa registro de usuario en firebase con email y password

---
+++ authapp/lib/app/data/repositories_impl/sign_up_repository_impl.dart
@@ -0,0 +1,23 @@
+import 'package:authapp/app/domain/inputs/sign_up.dart';
+import 'package:authapp/app/domain/repositories/sign_up_repository.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class SignUpRepositoryImpl implements SignUpRepository {
+  final FirebaseAuth _auth;
+
+  SignUpRepositoryImpl(this._auth);
+
+  @override
+  Future<SignUpResponse> register(SignUpData data) async {
+    try {
+      final UserCredential userCredential =
+          await _auth.createUserWithEmailAndPassword(
+              email: data.email, password: data.password);
+      await userCredential.user!
+          .updateDisplayName('${data.name} ${data.lastname}');
+      return SignUpResponse(null, userCredential.user!);
+    } on FirebaseAuthException catch (e) {
+      return SignUpResponse(e.code, null);
+    }
+  }
+}
