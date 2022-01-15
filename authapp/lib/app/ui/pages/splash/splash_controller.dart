80e75550177f39305e96f72330260edc3d0ec132
daniocen777 <danielochoa@outlook.com>
2022-01-15 09:15:17 -0500

    Controlador de la página Splash que verifica sesión (si se tiene una activa)

--- authapp/lib/app/ui/pages/splash/splash_controller.dart
+++ authapp/lib/app/ui/pages/splash/splash_controller.dart
@@ -1,3 +1,22 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_meedu/meedu.dart';
+
+import 'package:authapp/app/domain/repositories/authentication_repository.dart';
+
 class SplashController {
-  
+  final _authRepository = Get.i.find<AuthenticationRepository>();
+
+  SplashController() {
+    _init();
+  }
+
+  void _init() async {
+    final User? user = await _authRepository.user;
+    if (user != null) {
+      // Se tiene sesión activa
+      print("sesión activa");
+    } else {
+      print("sesión inactiva");
+    }
+  }
 }
