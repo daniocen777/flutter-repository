388d31b28aa4445b83aee868bf3bf3d2dd668025
daniocen777 <danielochoa@outlook.com>
2022-01-15 09:52:02 -0500

    Escuchando el cambio de ruta y navegar a otra

--- authapp/lib/app/ui/pages/splash/splash_page.dart
+++ authapp/lib/app/ui/pages/splash/splash_page.dart
@@ -1,12 +1,33 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_meedu/flutter_meedu.dart';
+import 'package:flutter_meedu/router.dart' as router;
+
+import 'package:authapp/app/ui/pages/splash/splash_controller.dart';
+
+// Para escuchar algún cambio
+final splashProvider = SimpleProvider((_) => SplashController());
+
 class SplashPage extends StatelessWidget {
   const SplashPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(child: CircularProgressIndicator())
+    return ProviderListener<SplashController>(
+      provider: splashProvider,
+      onChange: (_, controller) {
+        final routeName = controller.routeName;
+        if (routeName != null) {
+          router.pushReplacementNamed(routeName);
+        }
+      },
+      builder: (_, __) {
+        return const Scaffold(
+          body: Center(
+            child: CircularProgressIndicator(),
+          ),
+        );
+      },
     );
   }
 }
