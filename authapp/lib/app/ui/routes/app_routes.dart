8f842713a15056f91dcf4fc5a6db5862c2ff8060
daniocen777 <danielochoa@outlook.com>
2022-01-15 09:48:09 -0500

    Creando ruta para Logon

--- authapp/lib/app/ui/routes/app_routes.dart
+++ authapp/lib/app/ui/routes/app_routes.dart
@@ -1,7 +1,10 @@
+import 'package:flutter/material.dart';
+
+import 'package:authapp/app/ui/pages/login/login_page.dart';
 import 'package:authapp/app/ui/pages/splash/splash_page.dart';
 import 'package:authapp/app/ui/routes/routes.dart';
-import 'package:flutter/widgets.dart' show Widget, BuildContext;
 
 Map<String, Widget Function(BuildContext)> get appRoutes => {
       Routes.splash: (_) => const SplashPage(),
+      Routes.login: (_) => const LoginPage(),
     };
