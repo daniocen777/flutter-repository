97b530f48f71a241e98e31a0f0bec5e9c60fbc0f
daniocen777 <danielochoa@outlook.com>
2022-01-15 10:31:43 -0500

    Injectando nueva clase para registro de usuario en firebase usando email y password

--- authapp/lib/app/inject_dependencies.dart
+++ authapp/lib/app/inject_dependencies.dart
@@ -1,12 +1,20 @@
 /* Inyectar dependencias (crear instancias) */
 import 'package:authapp/app/data/repositories_impl/authentication_repository_impl.dart';
+import 'package:authapp/app/data/repositories_impl/sign_up_repository_impl.dart';
 import 'package:authapp/app/domain/repositories/authentication_repository.dart';
+import 'package:authapp/app/domain/repositories/sign_up_repository.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_meedu/meedu.dart';
 
 void injectDependencies() {
   /* inject the dependency as a singleton */
+  /* Para la sesión */
   // Crear instancia solo cuando se necesite
   Get.i.lazyPut<AuthenticationRepository>(
       () => AuthenticationRepositoryImpl(FirebaseAuth.instance));
+
+  /* Para el regstro de nuevo usuario firebase (email, password) */
+  // Crear instancia solo cuando se necesite
+  Get.i.lazyPut<SignUpRepository>(
+      () => SignUpRepositoryImpl(FirebaseAuth.instance));
 }
