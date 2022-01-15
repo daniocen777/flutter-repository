260c0b9b23846e571652598bad2b91cf4d63b5cd
daniocen777 <danielochoa@outlook.com>
2022-01-15 09:14:01 -0500

    Creando instancia peresoza para Firebase authentication

--- authapp/lib/app/inject_dependencies.dart
+++ authapp/lib/app/inject_dependencies.dart
@@ -1,5 +1,12 @@
 /* Inyectar dependencias (crear instancias) */
+import 'package:authapp/app/data/repositories_impl/authentication_repository_impl.dart';
+import 'package:authapp/app/domain/repositories/authentication_repository.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_meedu/meedu.dart';
 
 void injectDependencies() {
-  
+  /* inject the dependency as a singleton */
+  // Crear instancia solo cuando se necesite
+  Get.i.lazyPut<AuthenticationRepository>(
+      () => AuthenticationRepositoryImpl(FirebaseAuth.instance));
 }
