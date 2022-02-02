f0e287e3d59999b50c680c81db545e21e7c73309
daniocen777 <danielochoa@outlook.com>
2022-02-02 08:59:17 -0500

    Iniciando UI de registro de usuario

--- authapp/lib/app/ui/pages/login/login_page.dart
+++ authapp/lib/app/ui/pages/login/login_page.dart
@@ -1,12 +1,27 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_meedu/router.dart' as router;
+
+import 'package:authapp/app/ui/routes/routes.dart';
+
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: SafeArea(
+        child: SizedBox(
+          width: double.infinity,
+          child: Column(
+            children: [
+              ElevatedButton(
+                  onPressed: () => router.pushNamed(Routes.register),
+                  child: const Text('Sign Up'))
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
