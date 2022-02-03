78d00b1dfec85372ff8cd664dd426d36b88d0c83
daniocen777 <danielochoa@outlook.com>
2022-02-03 09:48:59 -0500

    Agregando ruta para registro

--- authapp/lib/app/ui/routes/app_routes.dart
+++ authapp/lib/app/ui/routes/app_routes.dart
@@ -1,3 +1,4 @@
+import 'package:authapp/app/ui/pages/home/home_page.dart';
 import 'package:authapp/app/ui/pages/register/register_page.dart';
 import 'package:flutter/material.dart';
 
@@ -8,5 +9,6 @@ import 'package:authapp/app/ui/routes/routes.dart';
 Map<String, Widget Function(BuildContext)> get appRoutes => {
       Routes.splash: (_) => const SplashPage(),
       Routes.login: (_) => const LoginPage(),
-      Routes.register: (_) => const RegisterPage()
+      Routes.register: (_) => const RegisterPage(),
+      Routes.home: (_) => const HomePage()
     };
