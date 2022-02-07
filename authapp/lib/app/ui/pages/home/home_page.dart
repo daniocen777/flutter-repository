07fbb46c543f7960182f17e31fff42616396910d
daniocen777 <danielochoa@outlook.com>
2022-02-07 08:41:31 -0500

    Cerrar sesión en página home

--- authapp/lib/app/ui/pages/home/home_page.dart
+++ authapp/lib/app/ui/pages/home/home_page.dart
@@ -1,5 +1,11 @@
+import 'package:authapp/app/ui/routes/routes.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import 'package:flutter_meedu/meedu.dart';
+import 'package:flutter_meedu/router.dart' as router;
+
+import 'package:authapp/app/domain/repositories/authentication_repository.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -15,7 +21,10 @@ class HomePage extends StatelessWidget {
             const SizedBox(height: 20.0),
             CupertinoButton(
               child: const Text('Cerrar sesión'),
-              onPressed: () {},
+              onPressed: () {
+                Get.i.find<AuthenticationRepository>().signOut();
+                router.pushNamedAndRemoveUntil(Routes.login);
+              },
             )
           ],
         ),
