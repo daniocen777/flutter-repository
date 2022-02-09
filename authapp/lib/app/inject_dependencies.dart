056ce1052bf5cd340b5f18d355549e434fc95c8a
daniocen777 <danielochoa@outlook.com>
2022-02-09 10:38:05 -0500

    Injectando dependencia para el repositprio Account

--- authapp/lib/app/inject_dependencies.dart
+++ authapp/lib/app/inject_dependencies.dart
@@ -1,10 +1,13 @@
 /* Inyectar dependencias (crear instancias) */
-import 'package:authapp/app/data/repositories_impl/authentication_repository_impl.dart';
-import 'package:authapp/app/data/repositories_impl/sign_up_repository_impl.dart';
-import 'package:authapp/app/domain/repositories/authentication_repository.dart';
-import 'package:authapp/app/domain/repositories/sign_up_repository.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_meedu/meedu.dart';
+
+import 'package:authapp/app/data/repositories_impl/account_repository_impl.dart';
+import 'package:authapp/app/data/repositories_impl/authentication_repository_impl.dart';
+import 'package:authapp/app/data/repositories_impl/sign_up_repository_impl.dart';
+import 'package:authapp/app/domain/repositories/account_repository.dart';
+import 'package:authapp/app/domain/repositories/authentication_repository.dart';
+import 'package:authapp/app/domain/repositories/sign_up_repository.dart';
 
 void injectDependencies() {
   /* inject the dependency as a singleton */
@@ -17,4 +20,8 @@ void injectDependencies() {
   // Crear instancia solo cuando se necesite
   Get.i.lazyPut<SignUpRepository>(
       () => SignUpRepositoryImpl(FirebaseAuth.instance));
+
+  /* Para la cuenta (actualizar nombre de usuario) */
+  Get.i.lazyPut<AccountRepository>(
+      () => AccountRepositoryImpl(FirebaseAuth.instance));
 }
