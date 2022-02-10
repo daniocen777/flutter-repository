55c8b82e4e6cb482cbb98882a917c11d5d02b0bf
daniocen777 <danielochoa@outlook.com>
2022-02-10 09:07:28 -0500

    Modificando página para escuchar los cambios del tema

--- authapp/lib/app/my_app.dart
+++ authapp/lib/app/my_app.dart
@@ -1,4 +1,6 @@
+import 'package:authapp/app/ui/global_controllers/theme_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_meedu/flutter_meedu.dart';
 
 import 'package:flutter_meedu/router.dart' as router;
 
@@ -10,16 +12,25 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      navigatorKey: router.navigatorKey,
-      title: 'Flutter Auth',
-      initialRoute: Routes.splash,
-      navigatorObservers: [
-        // Pendiente de cambios en las rutas (observa las notificaciones)
-        router.observer
-      ],
-      routes: appRoutes,
+    // Usar Consumer para escuchar el cambio de tema
+    return Consumer(
+      builder: (BuildContext context, BuilderRef ref, Widget? child) {
+        final themeController = ref.watch(themeProvider); // esuchar los cambios
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          navigatorKey: router.navigatorKey,
+          title: 'Flutter Auth',
+          initialRoute: Routes.splash,
+          themeMode: themeController.themeMode,
+          darkTheme: ThemeData.dark(),
+          theme: ThemeData.light(),
+          navigatorObservers: [
+            // Pendiente de cambios en las rutas (observa las notificaciones)
+            router.observer
+          ],
+          routes: appRoutes,
+        );
+      },
     );
   }
 }
