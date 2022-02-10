9df9c61228c85911ea9c1f004b94f2ee991fbdd6
daniocen777 <danielochoa@outlook.com>
2022-02-10 10:15:51 -0500

    Agregando función para modificar nombre del usuario

--- authapp/lib/app/ui/global_controllers/session_controller.dart
+++ authapp/lib/app/ui/global_controllers/session_controller.dart
@@ -1,5 +1,6 @@
 /* Estados globales para la app */
 
+import 'package:authapp/app/domain/repositories/account_repository.dart';
 import 'package:authapp/app/domain/repositories/authentication_repository.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_meedu/meedu.dart';
@@ -9,6 +10,8 @@ class SessionController extends SimpleNotifier {
   User? get user => _user;
   // Ya se tiene metodo signOut
   final _auth = Get.i.find<AuthenticationRepository>();
+  // Para el cambio de nombre
+  final _account = Get.i.find<AccountRepository>();
 
   /* dispose => EL controlador se autodestruye al ir a otra página != al controlador.
   Para persistir esl estado (global) autoDispose: false */
@@ -20,6 +23,15 @@ class SessionController extends SimpleNotifier {
   void setUser(User user) {
     _user = user;
     notify();
+  }
+
+  Future<User?> updateDisplayName(String value) async {
+    final user = await _account.updateDisplayName(value);
+    if (user != null) {
+      _user = user;
+      notify();
+    }
+    return user;
   }
 
   Future<void> signOut() async {
