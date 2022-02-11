115d62b6857ec2bea0384810efd708dfd8f7369d
daniocen777 <danielochoa@outlook.com>
2022-02-11 09:06:50 -0500

    Modificando controlador del tema para agregar preferencias

--- authapp/lib/app/ui/global_controllers/theme_controller.dart
+++ authapp/lib/app/ui/global_controllers/theme_controller.dart
@@ -1,8 +1,15 @@
+import 'package:authapp/app/domain/repositories/preferences_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_meedu/meedu.dart';
 
 class ThemeController extends SimpleNotifier {
-  ThemeMode _themeMode = ThemeMode.light; // tema por defecto
+  late ThemeMode _themeMode;
+  final _preferencesRepository = Get.i.find<PreferencesRepository>();
+
+  ThemeController() {
+    _themeMode =
+        _preferencesRepository.isDarkMode ? ThemeMode.dark : ThemeMode.light;
+  }
 
   ThemeMode get themeMode => _themeMode;
   bool get isThemeDark => _themeMode == ThemeMode.dark;
@@ -10,8 +17,10 @@ class ThemeController extends SimpleNotifier {
   void toggle() {
     if (_themeMode == ThemeMode.light) {
       _themeMode = ThemeMode.dark;
+      _preferencesRepository.darkMode(true); // Guardando preferencias del tema
     } else {
       _themeMode = ThemeMode.light;
+      _preferencesRepository.darkMode(false); // Guardando preferencias del tema
     }
     notify();
   }
