711efb850ec09f4f9296055ea655e1bef6dfc2a6
daniocen777 <danielochoa@outlook.com>
2022-02-13 10:06:54 -0500

    Modificando para agregar tema

--- authapp/lib/app/ui/global_widgets/dialogs/show_input_dialog.dart
+++ authapp/lib/app/ui/global_widgets/dialogs/show_input_dialog.dart
@@ -1,16 +1,22 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../../utils/dark_mode_extension.dart';
 
 Future<String?> showInputDialog(BuildContext context,
     {String? title, String? initialValue}) async {
   String value = initialValue ?? '';
-  TextEditingController textEditingController = TextEditingController();
-  textEditingController.text = value;
+  final isDarkMode = context.isThemeDarkMode;
   final result = await showCupertinoDialog<String>(
       context: context,
       builder: (BuildContext context) => CupertinoAlertDialog(
             title: title != null ? Text(title) : null,
             content: CupertinoTextField(
-              controller: textEditingController,
+              controller: TextEditingController()..text = initialValue ?? '',
+              style: TextStyle(color: isDarkMode ? Colors.white : Colors.black),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5.0),
+                  color: isDarkMode ? const Color(0xff29434e) : Colors.black12),
               onChanged: (text) {
                 value = text;
               },
@@ -34,9 +40,6 @@ Future<String?> showInputDialog(BuildContext context,
               )
             ],
           ));
-
-  // Libear controldor
-  textEditingController.dispose();
 
   if (result != null && result.trim().isEmpty) {
     return null; // Devolver null si result son espacios en blanco
