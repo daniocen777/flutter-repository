06ecdae0e63954ed252ea1cd5c0600149a35b70d
daniocen777 <danielochoa@outlook.com>
2023-05-21 16:34:26 -0500

    Importando assets y creadno página de Facebook - inicio

---
+++ my_widgets/android/app/src/main/kotlin/app/danicode/my_widgets/MainActivity.kt
@@ -0,0 +1,6 @@
+package app.danicode.my_widgets
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ my_widgets/lib/facebook_ui/facebook_ui.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FacebookUi extends StatelessWidget {
+  const FacebookUi({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
