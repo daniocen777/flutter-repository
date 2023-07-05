e9782ea1847e23d58fd715216991ec909d0eb46b
daniocen777 <danielochoa@outlook.com>
2023-07-05 10:01:15 -0500

    Carpetas de proyecto con clean architecture

---
+++ tv_clean/android/app/src/main/kotlin/app/danicode/tv_clean/MainActivity.kt
@@ -0,0 +1,6 @@
+package app.danicode.tv_clean
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ tv_clean/lib/main.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'package:tv_clean/app/my_app.dart';
+
+void main() => runApp(const MyApp());
