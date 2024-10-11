bed18cb935ce6940040bfb6ed26e969286dbcef8
daniocen777 <danielochoa@outlook.com>
2024-10-11 15:43:01 -0500

    Se creó el tema, utilitarios, paquetes, etc. Se creó un splash con el paquete flutter_native_splash

---
+++ store_clean/android/app/src/main/kotlin/com/danicode/store_clean/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.danicode.store_clean
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ store_clean/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+import 'utils/theme/theme.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Ecommerce App',
+      themeMode: ThemeMode.system,
+      theme: TAppTheme.lightTheme,
+      darkTheme: TAppTheme.darkTheme,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Material App Bar'),
+        ),
+        body: const Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
